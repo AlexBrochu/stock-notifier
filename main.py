a5b1c29d34cd5a21110b0887297e41c82d9bd0af
Alexandre Brochu <alexbrochuu@gmail.com>
2020-10-04 14:49:36 -0400

    refactor code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from load_config import load_stocks as ld
-
+from extractor.load_config import load_stocks as ld
+import extractor.extractor as e
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,14 +8,12 @@ from datetime import datetime
 from pandas_datareader import data as pdr
 
 import yfinance as yf
-msft = yf.Ticker("BPY-UN.TO")
-
-# get stock info
-print(msft.info)
-#print(msft.history(period="max"))
-
-
-#print(ac)
 
 data = ld()
-print(data['stocks'])
+
+portfolio = e.build_portfolio(data)
+# extract portfolio stats 
+portfolio_stats = e.extract_portfolio_stats(portfolio)
+
+e.write_info_to_file(portfolio_stats)
+e.load_info_from_file()
